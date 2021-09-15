59751a81d47d270b9c6f7f6f6118f335a5f5489b
chenans2 <anson96ca@gmail.com>
2021-09-14 20:57:36 -0400

    Use async await

--- client/src/App.js
+++ client/src/App.js
@@ -36,8 +36,10 @@ class App extends Component {
 
   getNowPlaying() {
     spotifyApi.getMyCurrentPlaybackState().then((response) => {
-      if (response.item === undefined) {
-        this.setState({ nowPlaying: { name: 'None' } });
+      if (!response.item) {
+        this.setState({
+          nowPlaying: { name: 'None' }
+        });
       } else {
         this.setState({
           nowPlaying: {
@@ -51,66 +53,69 @@ class App extends Component {
     });
   }
 
-  getSongID() {
-    const song = this.state.nowPlaying.name;
-    const parsedSong = song
+  async getSongID() {
+    const songName = this.state.nowPlaying.name;
+    const parsedSongName = songName
       .replace('Bonus Track', '')
       .replace('Radio Edit', '');
-    const geniusApiUrl = `https://api.genius.com/search?q=${parsedSong}
+
+    const geniusApiUrl = `https://api.genius.com/search?q=${parsedSongName}
       ${this.state.nowPlaying.artist}&access_token=${geniusToken}`;
 
-    fetch(geniusApiUrl, { method: 'GET' })
-      .then((response) => {
-        if (response.ok) return response.text();
-        throw new Error('Could not get json ...');
-      })
-      .then((json) => {
-        const songInfo = JSON.parse(json);
-        if (songInfo.response.hits[0] === undefined) {
-          // no search results
-          this.setState({
-            nowPlaying: {
-              ...this.state.nowPlaying,
-              lyrics: '[no lyrics found]'
-            }
-          });
-        } else {
-          // get url from first hit and scrape the HTML
-          const geniusUrl = songInfo.response.hits[0].result.url;
-          fetch('/scrape', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ url: geniusUrl })
-          })
-            .then((htmlResponse) => {
-              if (htmlResponse.ok) return htmlResponse.json();
-              throw new Error('Could not get HTML ...');
-            })
-            .then((value) => {
-              this.setState({
-                nowPlaying: {
-                  ...this.state.nowPlaying,
-                  lyrics: value.lyrics
-                }
-              });
-            });
-        }
+    try {
+      const geniusResponse = await fetch(geniusApiUrl, { method: 'GET' });
+      const songInfo = await geniusResponse.json();
+      if (songInfo.response.hits[0]) {
+        // get url from first hit and scrape the HTML
+        const geniusUrl = songInfo.response.hits[0].result.url;
+        const lyricsInfo = await this.scrapeLyrics(geniusUrl);
+        this.setState({
+          nowPlaying: {
+            ...this.state.nowPlaying,
+            lyrics: lyricsInfo.lyrics
+          }
+        });
+      } else {
+        // no search results
+        this.setState({
+          nowPlaying: {
+            ...this.state.nowPlaying,
+            lyrics: '[no lyrics found]'
+          }
+        });
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  async scrapeLyrics(geniusUrl) {
+    try {
+      const scrapeResponse = await fetch('/scrape', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ url: geniusUrl })
       });
+      return scrapeResponse.json();
+    } catch (err) {
+      console.log(err);
+    }
   }
 
   render() {
     return (
       <div className="App">
         {!this.state.loggedIn && (
-          <a href="http://localhost:8888" class="btn btn-green">
+          <a href="http://localhost:8888" className="btn btn-green">
             Log in with Spotify
           </a>
         )}
         {this.state.loggedIn && (
           <div>
-            <button onClick={() => this.getNowPlaying()} class="btn btn-green">
+            <button
+              onClick={() => this.getNowPlaying()}
+              className="btn btn-green"
+            >
               Check Now Playing
             </button>
             <h3>Now Playing: {this.state.nowPlaying.name}</h3>
